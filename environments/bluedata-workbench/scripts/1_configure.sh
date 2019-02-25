247794b01f1111191f399bad39280c68bc456342
Ben Hall <ben@benhall.me.uk>
2019-02-25 11:28:57 +0000

    Other packages that could be pre-installed

--- environments/bluedata-workbench/scripts/1_configure.sh
+++ environments/bluedata-workbench/scripts/1_configure.sh
@@ -1,8 +1,20 @@
+yum install -y python-pip
+pip install --upgrade setuptools
+pip install --upgrade requests
+pip install --upgrade argparse
+
 pip install --upgrade pip
 pip install --upgrade bdworkbench
+yum install -y epel-release
+
+yum install -y python-requests python-setuptools python-argparse python-pip
+
 yum install wget -y
 wget https://www.dropbox.com/s/xyv64z1xt9l6lw7/bdcatalog-centos7-bluedata-cdh514_centos7x-1.7.tar
 tar xvf bdcatalog-centos7-bluedata-cdh514_centos7x-1.7.tar
 chmod +x bdcatalog-centos7-bluedata-cdh514_centos7x-1.7.bin
 
 
+wget https://www.dropbox.com/s/wbnr83q26przbs6/appconfig.zip
+yum install unzip -y
+unzip appconfig.zip
