db741abbe39a241c5095bccd4dd91c117a1f6108
Ben Hall <ben@benhall.me.uk>
2019-05-06 12:33:30 +0100

    Add new bin files

--- environments/bluedata-workbench/build/1_configure.sh
+++ environments/bluedata-workbench/build/1_configure.sh
@@ -14,6 +14,13 @@ wget https://www.dropbox.com/s/xyv64z1xt9l6lw7/bdcatalog-centos7-bluedata-cdh514
 tar xvf bdcatalog-centos7-bluedata-cdh514_centos7x-1.7.tar
 chmod +x bdcatalog-centos7-bluedata-cdh514_centos7x-1.7.bin
 
+wget https://www.dropbox.com/s/r4m3bba2qtueu5q/bdcatalog-centos7-bluedata-spark221-1.0.bin.tar
+tar xvf bdcatalog-centos7-bluedata-spark221-1.0.bin.tar
+chmod +x bdcatalog-centos7-bluedata-spark221-1.0.bin
+
+wget https://www.dropbox.com/s/2mh0pzsq1092xr8/bdcatalog-centos7-bluedata-hdp25-ambari-3.3.bin.tar.gz
+tar xvf bdcatalog-centos7-bluedata-hdp25-ambari-3.3.bin.tar.gz
+chmod +x bdcatalog-centos7-bluedata-hdp25-ambari-3.3.bin
 
 wget https://www.dropbox.com/s/wbnr83q26przbs6/appconfig.zip
 yum install unzip -y
